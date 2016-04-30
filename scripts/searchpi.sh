92a00bb282bd75e5a14eb76a3d2055090125b221
Toon Meynen <toonmeynen@Toons-MacBook-Pro.local>
2016-04-30 13:58:46 +0200

    Add scripts folder with discovery script.

---
+++ scripts/searchpi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+IP=`ifconfig |grep --color -e 'inet 192.*' |cut -f 2 -d ' '`
+NETWORK=`echo $IP |sed 's/[1-9]*$/0/g'`
+sudo nmap -sP $NETWORK/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'
