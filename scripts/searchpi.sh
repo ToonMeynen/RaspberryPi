9ba857b443f4bac524e2e2c0b9280024c5c93ff7
Toon Meynen <toonmeynen@Shwans.local>
2016-10-29 11:31:20 +0200

    refactor script name and startup script

--- scripts/searchpi.sh
+++ scripts/searchpi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#find your pi on the network
 IP=`ifconfig |grep --color -e 'inet 192.*' |cut -f 2 -d ' '`
 NETWORK=`echo $IP |sed 's/[1-9]*$/0/g'`
 sudo nmap -sP $NETWORK/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'
